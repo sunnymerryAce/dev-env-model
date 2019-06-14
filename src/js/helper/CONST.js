8b9d5475ee739238ac4c09c1a7198e65237ce119
sunnymerryAce <y.hirano.0104@gmail.com>
2019-06-14 15:36:54 +0900

    [fix] CONST

--- src/js/helper/CONST.js
+++ src/js/helper/CONST.js
@@ -1,13 +1,12 @@
 /**
- * Constant Class
+ * Constants
  */
 const CONST = {
   // Events
-  EVENT_LOAD_COMPLETE: 'loadComplete',
-  EVENT_OPEN_MODAL: 'openModal',
-  EVENT_FILTER_STORIES: 'filterStories',
-  TEST: {
-    CLASS_NAME: 'class',
+  EVENT: {
+    LOAD_COMPLETE: 'loadComplete',
+    OPEN_MODAL: 'openModal',
+    FILTER_STORIES: 'filterStories',
   },
 };
 
